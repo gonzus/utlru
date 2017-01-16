3787b53aac46d41eeb53a064b012b9af9d079587
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2017-01-16 08:43:05 +0100

    Get rid of trace printing, use NULL instead of 0

--- lru.c
+++ lru.c
@@ -1,5 +1,4 @@
 #include <assert.h>
-#include <stdio.h>
 #include "gmem.h"
 #include "lru.h"
 
@@ -9,14 +8,13 @@ Cache* cache_build(pTHX_ int capacity)
     GMEM_NEW(cache, Cache*, sizeof(Cache));
     if (cache) {
         cache->capacity = capacity;
-        cache->data = 0;
+        cache->data = NULL;
     }
     return cache;
 }
 
 void cache_destroy(pTHX_ Cache* cache)
 {
-    /* fprintf(stderr, "LOG destroying cache for %d elements\n", cache->capacity); */
     cache_clear(aTHX_ cache);
 #if defined(GMEM_CHECK)
     assert(HASH_COUNT(cache->data) == 0);
@@ -36,27 +34,24 @@ int cache_capacity(pTHX_ Cache* cache)
 
 void cache_clear(pTHX_ Cache* cache)
 {
-    /* fprintf(stderr, "LOG clearing cache for %d elements\n", cache->capacity); */
     HASH_CLEAR(hh, cache->data);
 }
 
 SV* cache_find(pTHX_ Cache* cache, SV* key)
 {
     STRLEN klen = 0;
-    char* kptr = 0;
+    char* kptr = NULL;
     kptr = SvPV(key, klen);
 
     CacheEntry* entry;
     HASH_FIND(hh, cache->data, kptr, klen, entry);
     if (!entry) {
-        /* fprintf(stderr, "LOG could not find key [%s] in cache\n", key); */
         return NULL;
     }
 
     /*
      * remove it; the subsequent add will throw it on the front of the list
      */
-    /* fprintf(stderr, "LOG found key [%s] in cache\n", key); */
     HASH_DELETE(hh, cache->data, entry);
     HASH_ADD_KEYPTR(hh, cache->data, kptr, klen, entry);
     return entry->val;
@@ -67,7 +62,7 @@ int cache_add(pTHX_ Cache* cache, SV* key, SV* val)
     /* do not use gmem for these elements,
      * they will be deleted internally by ut */
     STRLEN klen = 0;
-    char* kptr = 0;
+    char* kptr = NULL;
     kptr = SvPV(key, klen);
 
     CacheEntry* entry = (CacheEntry*) malloc(sizeof(CacheEntry));
@@ -83,7 +78,6 @@ int cache_add(pTHX_ Cache* cache, SV* key, SV* val)
     SvREFCNT_inc(val);
 
     HASH_ADD_KEYPTR(hh, cache->data, kptr, klen, entry);
-    /* fprintf(stderr, "LOG added key [%s] => [%s]\n", key, val); */
 
     /*
      * prune the cache to not exceed its size
@@ -96,7 +90,6 @@ int cache_add(pTHX_ Cache* cache, SV* key, SV* val)
              * prune the first entry; loop is based on insertion
              * order so this deletes the oldest item
              */
-            /* fprintf(stderr, "LOG removing key [%s]\n", entry->key); */
 
             /* decrement perl refcounts */
             SvREFCNT_dec(entry->key);
@@ -112,8 +105,8 @@ int cache_add(pTHX_ Cache* cache, SV* key, SV* val)
 
 void cache_iterate(pTHX_ Cache* cache, CacheVisitor visitor, void* arg)
 {
-    CacheEntry* entry = 0;
-    CacheEntry* tmp = 0;
+    CacheEntry* entry = NULL;
+    CacheEntry* tmp = NULL;
     HASH_ITER(hh, cache->data, entry, tmp) {
         visitor(cache, entry, arg);
     }
